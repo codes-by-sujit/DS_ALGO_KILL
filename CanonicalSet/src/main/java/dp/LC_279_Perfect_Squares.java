069cefbd422cf0fea42943f34ad28f8f93b82bdb
sujit <sujitmajumdar42@gmail.com>
2026-02-13 00:11:09 +0530

    279. Perfect Squares

---
+++ CanonicalSet/src/main/java/dp/LC_279_Perfect_Squares.java
@@ -0,0 +1,16 @@
+package dp;
+
+public class LC_279_Perfect_Squares {
+    public int numSquares(int n) {
+        int[]dp=new int[n+1];
+        for(int i=1;i<=n;i++){
+            dp[i]=Integer.MAX_VALUE;
+            for(int j=1;j*j<=i;j++){
+                if(dp[i-(j*j)]!=Integer.MAX_VALUE){
+                    dp[i]=Math.min(dp[i],1+dp[i-(j*j)]);
+                }
+            }
+        }
+        return dp[n];
+    }
+}
