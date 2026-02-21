94fb68c000c4c531d86e9a39055bc4ba107707d7
sujit <sujitmajumdar42@gmail.com>
2026-02-21 23:37:51 +0530

    1695. Maximum Erasure Value

---
+++ CanonicalSet/src/main/java/sliding_window/LC_1695_Maximum_Erasure_Value.java
@@ -0,0 +1,30 @@
+package sliding_window;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LC_1695_Maximum_Erasure_Value {
+    public int maximumUniqueSubarray(int[] nums) {
+        int i=0;
+        int j=0;
+        int sum=0;
+        int maxScore = Integer.MIN_VALUE;
+        Map<Integer,Integer> map = new HashMap<>();
+        for(;j<nums.length;j++){
+            int curr = nums[j];
+            sum+=curr;
+            map.put(curr,map.getOrDefault(curr,0)+1);
+
+            while(map.get(curr)>1){
+                int leftNum=nums[i];
+                sum-=leftNum;
+                map.put(leftNum,map.get(leftNum)-1);
+                if(map.get(leftNum)==0)
+                    map.remove(leftNum);
+                i++;
+            }
+            maxScore=Math.max(maxScore, sum);
+        }
+        return maxScore;
+    }
+}
