d659f4076d9e2b04d51aeaf69ec99684e59bcdf4
sujit <sujitmajumdar42@gmail.com>
2026-03-06 19:09:49 +0530

    215. Kth Largest Element in an Array

---
+++ CanonicalSet/src/main/java/heap/level1/LC_215_kth_Largest_Element.java
@@ -0,0 +1,15 @@
+package heap.level1;
+
+import java.util.PriorityQueue;
+
+public class LC_215_kth_Largest_Element {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int num:nums){
+            pq.offer(num);
+            if(pq.size()>k)
+                pq.poll();
+        }
+        return pq.peek();
+    }
+}
