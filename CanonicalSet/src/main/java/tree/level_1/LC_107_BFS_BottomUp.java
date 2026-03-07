7d0edfccfd445212c2601fdabfaecce45c049308
sujit <sujitmajumdar42@gmail.com>
2026-03-07 08:20:38 +0530

    107. Binary Tree Level Order Traversal II

---
+++ CanonicalSet/src/main/java/tree/level_1/LC_107_BFS_BottomUp.java
@@ -0,0 +1,38 @@
+package tree.level_1;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class LC_107_BFS_BottomUp {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(root==null) return result;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while(!queue.isEmpty()){
+            List<Integer> level=new ArrayList<>();
+            int n=queue.size();
+            for(int i=0;i<n;i++){
+                TreeNode temp=queue.poll();
+                level.add(temp.val);
+                if(temp.left!=null) queue.offer(temp.left);
+                if(temp.right!=null) queue.offer(temp.right);
+            }
+            result.addFirst(level);
+        }
+        return result;
+    }
+
+    @Test
+    public void tc(){
+        TreeNode root = new TreeNode(3);
+        root.left=new TreeNode(9);
+        root.right=new TreeNode(20);
+        System.out.println(this.levelOrderBottom(root));
+    }
+}
