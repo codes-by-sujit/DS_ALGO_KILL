621e7ad39cb61c3be04a1f22bd1df79227a1753e
sujit <sujitmajumdar42@gmail.com>
2026-03-07 07:17:28 +0530

    94. Binary Tree Inorder Traversal

---
+++ CanonicalSet/src/main/java/tree/level_1/LC_94_Inorder.java
@@ -0,0 +1,18 @@
+package tree.level_1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LC_94_Inorder {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result=new ArrayList<>();
+        inorder(root,result);
+        return result;
+    }
+    private void inorder(TreeNode root, List<Integer> result){
+        if(root==null) return;
+        inorder(root.left,result);
+        result.add(root.val);
+        inorder(root.right,result);
+    }
+}
